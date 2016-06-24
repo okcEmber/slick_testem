aa6b2e2e04dd1d422b795528bc3abbb692b34ba4
Ryan LaBouve <imryanlabouve@gmail.com>
2016-06-24 17:21:01 -0500

    Fix JS hint errors and reorg js file a little

--- blueprints/slick_testem/files/tests/slick-testem.js
+++ blueprints/slick_testem/files/tests/slick-testem.js
@@ -1,19 +1,28 @@
-const
-  log = console.log,
-  getFailing = function () {
-    return document.querySelectorAll ('.fail').length;
+(function(window, document, $) {
+  "use strict";
+  var SlickTestem = function() {
+    return {
+      hasNotifiedFailing: false,
+      isFailing: function() {
+        return document.querySelectorAll ('.fail').length > 0;
+      },
+
+      checkFailing: function () {
+        if(this.isFailing && !this.hasNotifiedFailing) {
+          $('#qunit-header').append ($('<div>epic fail!</div>'));
+          this.hasNotifiedFailing = true;
+        }
+      },
+
+      monitor: function() {
+        var self = this;
+        window.setInterval(function () {
+          self.checkFailing();
+        }, 300);
+      }
+    };
   };
 
-var hasFailed = false;
-function failMonitor () {
-  setTimeout (function () {
-    if (getFailing()) {
-      if (!hasFailed) $ ('#qunit-header').append ($('<div>epic fail!</div>'));
-      hasFailed = true;
-    }
-    failMonitor();
-  }, 300);
-}
-
-
-failMonitor();
+  var slickTestem = new SlickTestem();
+  slickTestem.monitor();
+})(window, document, $);
