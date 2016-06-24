d790ccd4debeef919f5dfde1b68dcc082ff8c28b
Ryan LaBouve <imryanlabouve@gmail.com>
2016-06-24 18:06:29 -0500

    Restyle failing indicator and add how many tests have run #12

--- blueprints/slick_testem/files/tests/slick-testem.js
+++ blueprints/slick_testem/files/tests/slick-testem.js
@@ -1,28 +1,52 @@
 (function(window, document, $) {
   "use strict";
   var SlickTestem = function() {
+    var $slickTestem = $(`
+      <div style="float:right;" id="slick-testem">
+        <span class="tests-ran">0</span> / <span class="tests-total">0</span><br>
+      </div>
+    `);
+
     return {
       hasNotifiedFailing: false,
+
+      setupHeader: function() {
+        $('#qunit-header').append($slickTestem);
+        $('.tests-total').text($('[id^="qunit-test-output"]').length);
+      },
+
       isFailing: function() {
-        return document.querySelectorAll ('.fail').length > 0;
+        return $('.fail').length > 0;
+      },
+
+      updateProgress: function() {
+        var selectors = ['[id^="qunit-test-output"].pass',
+          '[id^="qunit-test-output"].fail',
+          '[id^="qunit-test-output"].skip'];
+        $('.tests-ran').text($(selectors.join(', ')).length);
       },
 
       checkFailing: function () {
-        if(this.isFailing && !this.hasNotifiedFailing) {
-          $('#qunit-header').append ($('<div>epic fail!</div>'));
+        if(this.isFailing() && !this.hasNotifiedFailing) {
+          $('#slick-testem').addClass('failing');
           this.hasNotifiedFailing = true;
         }
       },
 
-      monitor: function() {
-        var self = this;
-        window.setInterval(function () {
-          self.checkFailing();
+      go: function() {
+        this.monitorTimer = window.setInterval(() => {
+          this.checkFailing();
+          this.updateProgress();
         }, 300);
+
+        this.setupTimer = window.setTimeout(() => {
+          this.setupHeader();
+        }, 1000); // lazy timer
       }
     };
   };
 
+  var a
   var slickTestem = new SlickTestem();
-  slickTestem.monitor();
+  slickTestem.go();
 })(window, document, $);
