164f27869576e29bf6f5a0ad01d4c71eb54f6fe9
Ryan LaBouve <imryanlabouve@gmail.com>
2016-06-24 13:12:07 -0500

    Spike on external js and css files

--- blueprints/slick_testem/index.js
+++ blueprints/slick_testem/index.js
@@ -8,8 +8,13 @@ module.exports = {
   afterInstall: function() {
     return this.insertIntoFile(
       'tests/index.html',
-      '    <script>alert("hello");</script>',
+      '    <script src="assets/slick-testem.js"></script>',
       { after: '<script src="assets/test-loader.js"></script>' + EOL }
-    );
+    ).then(function() {
+      return this.insertIntoFile(
+        'tests/index.html',
+        '    <link rel="stylesheet" href="assets/slick-testem.css"></script>',
+        { after: '<link rel="stylesheet" href="assets/test-support.css">' + EOL });
+    }.bind(this));
   }
 };
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
 module.exports = {
-  name: 'slick-testem'
+  name: 'slick-testem',
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+  postprocessTree: function(type, tree) {
+    if (type === 'all' && this.app.tests) {
+      var tests = this.treeGenerator(path.join(this.project.root, 'tests'));
+      var slickFiles = new Funnel(tests, {
+        files: ['slick-testem.js', 'slick-testem.css'],
+        srcDir: '/',
+        destDir: '/assets'
+      });
+
+      console.log(slickFiles);
+
+      return mergeTrees([tree, slickFiles], {
+        overwrite: true
+      });
+    } else {
+      return tree;
+    }
+  }
 };
