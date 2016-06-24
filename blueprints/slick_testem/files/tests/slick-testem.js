21461b8c46d83c1feb26d5b9fd84742e7f688d1f
cotterjd <jcotter@flogistix.com>
2016-06-24 13:57:14 -0500

    Issue #5 - detect failed tests

--- blueprints/slick_testem/files/tests/slick-testem.js
+++ blueprints/slick_testem/files/tests/slick-testem.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const
+  log = console.log,
+  getFailing = function () {
+    return document.querySelectorAll ('.fail').length;
+  };
+
+var hasFailed = false;
+function failMonitor () {
+  setTimeout (function () {
+    if (getFailing()) {
+      if (!hasFailed) $ ('#qunit-header').append ($('<div>epic fail!</div>'));
+      hasFailed = true;
+    }
+    failMonitor();
+  }, 300);
+}
+
+
+failMonitor();
--- tests/slick-testem.js
+++ tests/slick-testem.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const
+  log = console.log,
+  getFailing = function () {
+    return document.querySelectorAll ('.fail').length;
+  };
+
+var hasFailed = false;
+function doStuff () {
+  
+  setTimeout (function () {
+
+  if (getFailing()) {
+    if (!hasFailed) $ ('#qunit-header').append ($('<div>test!</div>'));
+    hasFailed = true;
+  }
+  console.log('hola world');
+    doStuff();
+  }, 300);
+}
+
+
+doStuff();
